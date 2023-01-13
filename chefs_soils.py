cca50ac9900315de8019ed88bf2ff72ef432b9f9
Roy Jeong <rjeong@vividsolutions.com>
2023-01-13 10:04:39 -0800

    chefs exception handle

--- chefs_soils.py
+++ chefs_soils.py
@@ -1,5 +1,6 @@
 # pylint: disable=line-too-long
 # pylint: disable=no-member
+# pylint: disable=unbalanced-tuple-unpacking
 """
 Retrieve CHEFS form data, overwrite it into AGOL CSVs and Layers,
 send notification email to subscribers who want to get information for the soil relocation
@@ -16,21 +17,22 @@ LOGLEVEL = os.getenv('LOGLEVEL')
 logging.basicConfig(level=LOGLEVEL, format='%(asctime)s [%(levelname)s] %(message)s')
 
 logging.info('Loading Submissions List...')
-submissionsJson, hvsJson, subscribersJson = submission_loader.load_submissions()
+submissionsJson, hvsJson, subscribersJson, chefsLoaded = submission_loader.load_submissions()
 
-logging.info('Creating source site, receiving site records...')
-sourceSites, srcRegDistDic, receivingSites, rcvRegDistDic, hvSites, hvRegDistDic = submission_mapper.map_sites(submissionsJson, hvsJson)
+if chefsLoaded:
+    logging.info('Creating source site, receiving site records...')
+    sourceSites, srcRegDistDic, receivingSites, rcvRegDistDic, hvSites, hvRegDistDic = submission_mapper.map_sites(submissionsJson, hvsJson)
 
-logging.info('Creating soil source / receiving / high volume site CSV...')
-csv_writer.site_csv_writer(sourceSites, receivingSites, hvSites)
+    logging.info('Creating soil source / receiving / high volume site CSV...')
+    csv_writer.site_csv_writer(sourceSites, receivingSites, hvSites)
 
-logging.info('Updating source / receiving / high volume site CSV and Layer in AGOL...')
-agol_updater.agol_items_overwrite()
+    logging.info('Updating source / receiving / high volume site CSV and Layer in AGOL...')
+    agol_updater.agol_items_overwrite()
 
-logging.info('Sending notification emails to subscribers...')
-email_sender.email_to_subscribers(subscribersJson, srcRegDistDic, rcvRegDistDic, hvRegDistDic)
+    logging.info('Sending notification emails to subscribers...')
+    email_sender.email_to_subscribers(subscribersJson, srcRegDistDic, rcvRegDistDic, hvRegDistDic)
 
-logging.info('Completed Soils data publishing')
+    logging.info('Completed Soils data publishing')
 
 #### to track the version of forms (Jan/12/2023)
 # CHEFS generates new vresion of forms when changes of data fields, manages data by each version
--- submission_loader.py
+++ submission_loader.py
@@ -6,7 +6,6 @@ import os
 import logging
 import helper
 
-
 CHEFS_SOILS_FORM_ID = os.getenv('CHEFS_SOILS_FORM_ID')
 CHEFS_SOILS_API_KEY = os.getenv('CHEFS_SOILS_API_KEY')
 CHEFS_HV_FORM_ID = os.getenv('CHEFS_HV_FORM_ID')
@@ -19,31 +18,45 @@ logging.basicConfig(level=LOGLEVEL, format='%(asctime)s [%(levelname)s] %(messag
 
 def load_submissions():
     """Load all site submissions from CHEFS"""
+    chefs_loaded = True
     logging.info('Loading Submissions List...')
+    submission_loaded = True
     submissions_json = helper.site_list(CHEFS_SOILS_FORM_ID, CHEFS_SOILS_API_KEY)
-    # if int(submissionsJson.get('status')) > 201:
-    #     logging.error("Loading Submissions List failed: %s %s, %s", submissionsJson['status'], submissionsJson['title'], submissionsJson['detail'])
-    logging.info("%s Submissions are retrived.", len(submissions_json))
+    if isinstance(submissions_json, dict):
+        if int(submissions_json.get('status')) > 201:
+            logging.error("Loading Submissions List failed: %s %s, %s", submissions_json['status'], submissions_json['title'], submissions_json['detail'])
+            submission_loaded = False
+    if submission_loaded:
+        logging.info("%s Submissions are retrived.", len(submissions_json))
     logging.debug(submissions_json)
     # logging.info('Loading Submission attributes and headers...')
     # soilsAttributes = helper.fetch_columns(CHEFS_SOILS_FORM_ID, CHEFS_SOILS_API_KEY)
 
     logging.info('Loading High Volume Sites list...')
+    hvs_loaded = True
     hvs_json = helper.site_list(CHEFS_HV_FORM_ID, CHEFS_HV_API_KEY)
-    # if int(hvsJson.get('status')) > 201:
-    #     logging.error("Loading High Volume Sites list failed: %s %s, %s", hvsJson['status'], hvsJson['title'], hvsJson['detail'])
-    logging.info("%s High Volume Sites are retrived.", len(hvs_json))
+    if isinstance(hvs_json, dict):
+        if int(hvs_json.get('status')) > 201:
+            logging.error("Loading High Volume Sites list failed: %s %s, %s", hvs_json['status'], hvs_json['title'], hvs_json['detail'])
+            hvs_loaded = False
+    if hvs_loaded:
+        logging.info("%s High Volume Sites are retrived.", len(hvs_json))
     logging.debug(hvs_json)
     # logging.info('Loading High Volume Sites attributes and headers...')
     # hvsAttributes = helper.fetch_columns(CHEFS_HV_FORM_ID, CHEFS_HV_API_KEY)
 
     logging.info('Loading submission subscribers list...')
+    subscribers_loaded = True
     subscribers_json = helper.site_list(CHEFS_MAIL_FORM_ID, CHEFS_MAIL_API_KEY)
-    # if int(subscribersJson.get('status')) > 201:
-    #     logging.error("Loading submission subscribers list failed: %s %s, %s", subscribersJson['status'], subscribersJson['title'], subscribersJson['detail'])
-    logging.info("%s Submission Subscribers are retrived.", len(subscribers_json))
+    if isinstance(subscribers_json, dict):
+        if int(subscribers_json.get('status')) > 201:
+            logging.error("Loading submission subscribers list failed: %s %s, %s", subscribers_json['status'], subscribers_json['title'], subscribers_json['detail'])
+            subscribers_loaded = False
+    if subscribers_loaded:
+        logging.info("%s Submission Subscribers are retrived.", len(subscribers_json))
     logging.debug(subscribers_json)
     # logging.info('Loading submission subscribers attributes and headers...')
     # subscribeAttributes = helper.fetch_columns(CHEFS_MAIL_FORM_ID, CHEFS_MAIL_API_KEY)
 
-    return submissions_json, hvs_json, subscribers_json
+    chefs_loaded = True if submission_loaded and hvs_loaded and subscribers_loaded else False
+    return submissions_json, hvs_json, subscribers_json, chefs_loaded
