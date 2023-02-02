e596ce30d3a90a0c9a18879ca41f9b23eb14b63a
Roy Jeong <rjeong@vividsolutions.com>
2023-02-01 17:06:02 -0800

    soil relocation form version -> v12

--- chefs_soils.py
+++ chefs_soils.py
@@ -39,8 +39,8 @@ if chefsLoaded:
 
     logging.info('Completed Soils data publishing')
 
-#### to track the version of forms (Jan/12/2023)
+#### to track the version of forms (Feb/01/2023)
 # CHEFS generates new vresion of forms when changes of data fields, manages data by each version
-# 1.soil relocation form version: v11
+# 1.soil relocation form version: v12
 # 2.high volume submission version v8
 # 3.subscriber form version: v9
